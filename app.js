e6c55c48f8965b7e9a3aa5592e94ecf1c4b8f60b
Kumari Anushka <anu.sb0340@gmail.com>
2021-08-07 02:26:44 +0530

    Added usefindandmodify

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // mongoDB config
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true, 'useFindAndModify': false })
     .then(() => {
         console.log('Connected to mongoDB.');
     })
