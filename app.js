7986475bd0f9c081f0b894a9009827006291d0e7
Kumari Anushka <anu.sb0340@gmail.com>
2021-08-07 02:16:19 +0530

    hosting fixes

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // mongoDB config
-mongoose.set('bufferCommands', false);
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true })
     .then(() => {
         console.log('Connected to mongoDB.');
