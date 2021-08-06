712a2b5987cdcbb8797bc7b86ae548b51f6c17d4
Kumari Anushka <anu.sb0340@gmail.com>
2021-08-07 02:11:30 +0530

    set buffer commands

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // mongoDB config
+mongoose.set('bufferCommands', false);
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true })
     .then(() => {
         console.log('Connected to mongoDB.');
